261578e19967254e245628cd88037ff90800fdd8
Prasanti <nalla.prasanti@gmail.com>
2023-01-19 18:47:10 +0000

    added few more changes to code

--- BusTimes.js
+++ BusTimes.js
@@ -3,15 +3,15 @@ export async function getBusTimes(stopCodeData, noOfBusStops) {
         const busStopId = stopCodeData.stopPoints[i].id;
         let tflBusStopsAPIResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${busStopId}/Arrivals`);
         const stopname = stopCodeData.stopPoints[i].commonName;
-        console.log("---------------------------------")
+        console.log("---------------------------------");
         console.log(`Buses at ${stopname} stop`);
-        console.log("---------------------------------")
+        console.log("---------------------------------");
         if (tflBusStopsAPIResponse.ok) {
             const tflBusStops = await tflBusStopsAPIResponse.json();
             for (let i = 0; i < 5; i++) {
                 const time = tflBusStops[i].expectedArrival.slice(11, 19);
                 //console.log(tflBusStops);
-                console.log(`Bus${tflBusStops[i].lineName}   ${time}   ${tflBusStops[i].destinationName}`);
+                console.log(`${tflBusStops[i].lineName}   ${time}   ${tflBusStops[i].destinationName}`);
             }
         }
         else {
